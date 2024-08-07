e6c87df1ea9ca7f07c4e4d3c5e7d7fca38c7cb73
Lemon <vladred2016@gmail.com>
2024-08-07 19:19:44 +0300

    feat: added Recipe.cs, Ingredient.cs;

--- Domain/Entities/Ingredient.cs
+++ Domain/Entities/Ingredient.cs
@@ -3,4 +3,6 @@
 public class Ingredient : BaseEntity
 {
     public string Name { get; set; }
+    public int? Quantity { get; set; }
+    public string? Unit { get; set; }
 }
--- Domain/Entities/Recipe.cs
+++ Domain/Entities/Recipe.cs
@@ -2,8 +2,10 @@
 
 public class Recipe : BaseEntity
 {
-    public string Title { get; set; } 
+    public string Title { get; set; }
+    public Uri Link { get; set; }
     public ICollection<Ingredient> Ingredient { get; set; }
     public string Hash { get; set; }
-
+    public int? PreparationTime { get; set; }
+    public int? Servings { get; set; }
 }
