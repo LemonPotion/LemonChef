13edaf9c10ec96f04ac0a6496aeaaa0a619e9100
Lemon <vladred2016@gmail.com>
2024-09-08 19:25:28 +0300

    feat: added IngredientMappingProfileTests.cs test, added Ignore() options in IngredientMappingProfile.cs;

--- Application/Mapping/IngredientMappingProfile.cs
+++ Application/Mapping/IngredientMappingProfile.cs
@@ -18,7 +18,12 @@ public class IngredientMappingProfile : Profile
                 dto.Quantity,
                 dto.Unit,
                 dto.RecipeId
-            ));
+            ))
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest=> dest.Recipe, opt=> opt.Ignore())
+            .ForMember(dest=> dest.ModifiedOn, opt=> opt.Ignore())
+            .ForMember(dest=> dest.CreatedOn, opt=> opt.Ignore());
+        
         CreateMap<IngredientUpdateRequest, Ingredient>()
             .ConstructUsing(dto => new Ingredient(
                 dto.Id, 
@@ -26,25 +31,37 @@ public class IngredientMappingProfile : Profile
                 dto.Quantity,
                 dto.Unit,
                 dto.RecipeId
-            ));
+            ))
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest=> dest.Recipe, opt=> opt.Ignore())
+            .ForMember(dest=> dest.ModifiedOn, opt=> opt.Ignore())
+            .ForMember(dest=> dest.CreatedOn, opt=> opt.Ignore());
+        
         CreateMap<IngredientCreateResponse, Ingredient>()
             .ForMember(dest=> dest.Id, opt=> opt.MapFrom(src=>src.Id))
             .ForMember(dest=> dest.Quantity, opt=> opt.MapFrom(src=>src.Quantity))
             .ForMember(dest=> dest.RecipeId, opt=> opt.MapFrom(src=>src.RecipeId))
             .ForMember(dest=> dest.Name, opt=> opt.MapFrom(src=>src.Name))
-            .ForMember(dest=> dest.Unit, opt=> opt.MapFrom(src=>src.Unit));
+            .ForMember(dest=> dest.Unit, opt=> opt.MapFrom(src=>src.Unit))
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest=> dest.Recipe, opt=> opt.Ignore())
+            .ForMember(dest=> dest.ModifiedOn, opt=> opt.Ignore())
+            .ForMember(dest=> dest.CreatedOn, opt=> opt.Ignore());
+        
         CreateMap<Ingredient, IngredientCreateResponse>()
             .ForMember(dest=> dest.Id, opt=> opt.MapFrom(src=>src.Id))
             .ForMember(dest=> dest.Quantity, opt=> opt.MapFrom(src=>src.Quantity))
             .ForMember(dest=> dest.RecipeId, opt=> opt.MapFrom(src=>src.RecipeId))
             .ForMember(dest=> dest.Name, opt=> opt.MapFrom(src=>src.Name))
             .ForMember(dest=> dest.Unit, opt=> opt.MapFrom(src=>src.Unit));
+        
         CreateMap<Ingredient, IngredientUpdateResponse>()
             .ForMember(dest=> dest.Id, opt=> opt.MapFrom(src=>src.Id))
             .ForMember(dest=> dest.Quantity, opt=> opt.MapFrom(src=>src.Quantity))
             .ForMember(dest=> dest.RecipeId, opt=> opt.MapFrom(src=>src.RecipeId))
             .ForMember(dest=> dest.Name, opt=> opt.MapFrom(src=>src.Name))
             .ForMember(dest=> dest.Unit, opt=> opt.MapFrom(src=>src.Unit));
+        
         CreateMap<Ingredient, IngredientGetResponse>()
             .ForMember(dest=> dest.Id, opt=> opt.MapFrom(src=>src.Id))
             .ForMember(dest=> dest.Quantity, opt=> opt.MapFrom(src=>src.Quantity))
---
+++ Tests/MappingTests/IngredientMappingProfileTests.cs
@@ -0,0 +1,138 @@
+﻿using Application.Dto_s.Requests.Ingredient;
+using Application.Dto_s.Responses.Ingredient;
+using Application.Mapping;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Validations.Primitives;
+using FluentAssertions;
+
+namespace Tests.MappingTests;
+
+public class IngredientMappingProfileTests
+{
+    private readonly IMapper _mapper;
+    private readonly MapperConfiguration _configuration ;
+
+    public IngredientMappingProfileTests()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<IngredientMappingProfile>();
+        });
+        _configuration = config;
+        _mapper = config.CreateMapper();
+    }
+
+    [Fact]
+    public void Ingredient_MappingConfiguration_IsValid()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+
+    [Fact]
+    public void Should_Map_IngredientToIngredientCreateResponse()
+    {
+        var ingredient = new Ingredient
+        {
+            Id = Guid.NewGuid(),
+            CreatedOn = DateTime.UtcNow,
+            ModifiedOn = null,
+            Name = "Tomato",
+            Quantity = null,
+            RecipeId = Guid.NewGuid(),
+            Unit = null
+        };
+        var response = _mapper.Map<IngredientCreateResponse>(ingredient);
+
+        response.Should().BeEquivalentTo(ingredient, cfg => cfg
+            .Excluding(src => src.CreatedOn)
+            .Excluding(src => src.ModifiedOn)
+            .Excluding(src=> src.Recipe)
+            .ComparingByMembers<Ingredient>()
+            .ComparingByMembers<IngredientCreateResponse>());
+    }
+
+    [Fact]
+    public void Should_Map_IngredientCreateRequestToIngredient()
+    {
+        var request = new IngredientCreateRequest
+        {
+            Name = "Tomato",
+            Quantity = null,
+            RecipeId = Guid.NewGuid(),
+            Unit = null
+        };
+        var response = _mapper.Map<Ingredient>(request);
+        
+        response.Should().BeEquivalentTo(request, cfg => cfg
+            .ComparingByMembers<Ingredient>()
+            .ComparingByMembers<IngredientCreateRequest>());
+    }
+
+    [Fact]
+    public void Should_Map_IngredientToIngredientUpdateResponse()
+    {
+        var ingredient = new Ingredient
+        {
+            Id = Guid.NewGuid(),
+            CreatedOn = DateTime.UtcNow,
+            ModifiedOn = null,
+            Name = "Tomato",
+            Quantity = null,
+            RecipeId = Guid.NewGuid(),
+            Unit = null
+        };
+        
+        var response = _mapper.Map<IngredientUpdateResponse>(ingredient);
+
+        response.Should().BeEquivalentTo(ingredient, cfg => cfg
+            .Excluding(src=> src.CreatedOn)
+            .Excluding(src=> src.ModifiedOn)
+            .Excluding(src=> src.Recipe)
+            .ComparingByMembers<Ingredient>()
+            .ComparingByMembers<IngredientUpdateResponse>());
+    }
+
+    [Fact]
+    public void Should_Map_IngredientUpdateRequestToIngredient()
+    {
+        var request = new IngredientUpdateRequest
+        {
+            Name = "Tomato",
+            Quantity = null,
+            RecipeId = Guid.NewGuid(),
+            Unit = null,
+            Id = Guid.NewGuid()
+        };
+
+        var response = _mapper.Map<Ingredient>(request);
+
+        response.Should().BeEquivalentTo(request, cfg => cfg
+            .ComparingByMembers<IngredientUpdateRequest>()
+            .ComparingByMembers<Ingredient>());
+    }
+
+    [Fact]
+    public void Should_Map_IngredientToIngredientGetResponse()
+    {
+        var ingredient = new Ingredient
+        {
+            Id = Guid.NewGuid(),
+            CreatedOn = DateTime.UtcNow,
+            ModifiedOn = null,
+            Name = "Tomato",
+            Quantity = null,
+            RecipeId = Guid.NewGuid(),
+            Unit = null
+        };
+
+        var response = _mapper.Map<IngredientGetResponse>(ingredient);
+
+        response.Should().BeEquivalentTo(ingredient, cfg => cfg
+            .Excluding(src => src.CreatedOn)
+            .Excluding(src => src.ModifiedOn)
+            .Excluding(src => src.Recipe)
+            .ComparingByMembers<Ingredient>()
+            .ComparingByMembers<IngredientGetResponse>());
+    }
+}
